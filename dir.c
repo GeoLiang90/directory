b4f7f6eee9f2734d05e04f0c89859f62c19204e4
taniacao12 <tcaosu@stuy.edu>
2018-11-02 13:48:04 -0400

    edit dir.c

--- dir.c
+++ dir.c
@@ -1,18 +1,38 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 int main(){
   DIR * d;
+  struct stat info;
+  stat(filename, &info);
+
+  // print file size
+  int size = info.st_size;
+  printf(" File size: [%d]\n", size);
+
   d = opendir("./");
   struct dirent * entry;
-
-  while (entry){
+  printf("Directories:\n");
+  while (entry) {
+    if ( entry->d_type == DT_DIR) {
+      printf("\t%s \n", entry->d_name);
+    }
     entry = readdir(d);
-    printf(" %s \n", entry->d_name);
+  }
+  closedir(d);
+
+  d = opendir("./");
+  struct dirent * ent;
+  printf("Regular files:\n");
+  while (ent) {
+    if (ent->d_type == DT_REG) {
+      printf("\t%s \n", ent->d_name);
+      
+    }
+    ent = readdir(d);
   }
   closedir(d);
   return 0;
